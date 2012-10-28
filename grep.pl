fc4bd34960a121e84d69111e5e78f44c74641ecc
txor <txor@xino-xano.(none)>
2012-10-28 12:25:32 +0100

    Ahora es mas bonitao...

--- grep.pl
+++ grep.pl
@@ -4,12 +4,12 @@
 #
 
 my $VERSION = "0.1";
-my $HEADER = "$0 "."VERSION ".$VERSION."\n";
+my $HEADER = "$0 "."VERSION ".$VERSION;
 
 if (@ARGV == 0) {
 	print STDERR "$HEADER\n";
 	print STDERR "$0 needs at least one parameter!\n";
-	print STDERR "Try -h to see available options.";
+	print STDERR "Try -h to see available options.\n";
 	exit -1;
 }
 
