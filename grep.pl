341c79dc53124e57b89f8d32bf182170161b22e5
txor <txor@xino-xano.(none)>
2012-10-28 12:18:23 +0100

    Missatge d'error afegit.

--- grep.pl
+++ grep.pl
@@ -4,9 +4,14 @@
 #
 
 my $VERSION = "0.1";
+my $HEADER = "$0 "."VERSION ".$VERSION."\n";
 
 if (@ARGV == 0) {
-	print STDERR "$0 "."VERSION ".$VERSION."\n";
+	print STDERR "$HEADER\n";
+	print STDERR "$0 needs at least one parameter!\n";
 	exit -1;
+}
+elsif (@ARGV == 0) {
+
 }
 
