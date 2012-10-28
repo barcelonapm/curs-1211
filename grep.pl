762a133dcd3826b80f911b06ecdb1b377a2c34f3
txor <txor@xino-xano.(none)>
2012-10-28 12:23:12 +0100

    Tercera línia del cas sense arguments, porta un "-h" a dins.

--- grep.pl
+++ grep.pl
@@ -9,9 +9,7 @@ my $HEADER = "$0 "."VERSION ".$VERSION."\n";
 if (@ARGV == 0) {
 	print STDERR "$HEADER\n";
 	print STDERR "$0 needs at least one parameter!\n";
+	print STDERR "Try -h to see available options.";
 	exit -1;
-}
-elsif (@ARGV == 0) {
-
 }
 
