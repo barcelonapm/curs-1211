783ec5ac6783a71dd9d8db0f24d1371e580b256d
Alex Muntada <alexm@alexm.org>
2012-11-06 09:30:16 +0100

    llegim un fh i li apliquem el cb a les línies

--- Grep.pm
+++ Grep.pm
@@ -5,17 +5,25 @@ use warnings;
 
 use Exporter 'import';
 
-our $VERSION   = "0.01";
-our @EXPORT_OK = qw( scan_input );
+our $VERSION   = "0.02";
+our @EXPORT_OK = qw( scan_input match_line );
 
 sub scan_input {
-    # .
-    # .
-    # .
-    # to be completed
-    # .
-    # .
-    # .
+    my ( $filehandle, $callback ) = @_;
+
+    # for each line of $filehandle
+    # call match_line with $callback
+
+    # return: array of match_line results
+}
+
+sub match_line {
+    my ( $line_nr, $line_text, $callback ) = @_;
+
+    # if $callback returns true with $line_text
+    # return: hashref with keys match, text, line_nr
+
+    # return undef otherwise
 }
 
 # package success
