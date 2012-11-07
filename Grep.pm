abb0a1ba6631f494ad34d577454593c6d603fcef
Alex Muntada <alexm@alexm.org>
2012-11-07 21:46:04 +0100

    add match_text to Grep.pm barebones

--- Grep.pm
+++ Grep.pm
@@ -11,19 +11,39 @@ our @EXPORT_OK = qw( scan_input match_line );
 sub scan_input {
     my ( $filehandle, $callback ) = @_;
 
+    #
+    # TODO for students:
+    #
     # for each line of $filehandle
     # call match_line with $callback
-
+    #
     # return: array of match_line results
+    #
 }
 
 sub match_line {
     my ( $line_nr, $line_text, $callback ) = @_;
 
+    #
+    # TODO for students:
+    #
     # if $callback returns non-empty array with $line_text
     # return: hashref with keys match, text, line_nr
-
+    #
     # return undef otherwise
+    #
+}
+
+#
+# Text matching function (feel free to use)
+#
+# returns: first match or undef
+#
+sub match_text {
+    my ( $pattern, $content ) = @_;
+
+    my ($match) = $content =~ /($pattern)/;
+    return $match;
 }
 
 # package success
